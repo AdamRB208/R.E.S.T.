e0d20defab7f339697689e109c3e5dc73d133b44
AdamRB208 <adamrborup@gmail.com>
2025-03-12 14:36:22 -0600

    getting rats!

--- src/db/DbContext.js
+++ src/db/DbContext.js
@@ -1,12 +1,12 @@
 import mongoose from 'mongoose'
 import { AccountSchema } from '../models/Account.js'
 import { ValueSchema } from '../models/Value.js'
-import { RatsSChema } from '../models/Rats.js';
+import { RatsSchema } from '../models/Rats.js';
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
-  Rats = mongoose.model('Rat', RatsSChema);
+  Rats = mongoose.model('Rat', RatsSchema);
 }
 
 export const dbContext = new DbContext()
